bfcfac4f8da023ea8fb0fb9441e2edd36d3e0f78
Ramses Romero <ramses.romero.jr@gmail.com>
2023-12-03 00:28:40 -0500

    added calories to seperate window and styling

--- Calorie.py
+++ Calorie.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 class Calculator:
     def __init__(self):
         self.total_macros = [0, 0, 0, 0]
+        self.food_nutrients_dict = {}
 
         self.window = tk.Tk()
         self.food_label = ttk.Label(self.window, text="Food:")
@@ -41,6 +42,7 @@ class Calculator:
 
     def on_add_food(self):
 
+        food_name = self.food_entry.get()
         calories_value = self.calories_entry.get()
         protein_value = self.protein_entry.get()
         carbs_value = self.carbs_entry.get()
@@ -49,6 +51,8 @@ class Calculator:
         self.total_macros[1] += int(protein_value)
         self.total_macros[2] += int(carbs_value)
         self.total_macros[3] += int(fats_value)
+
+        self.food_nutrients_dict[food_name] = [calories_value, protein_value, carbs_value, fats_value]
 
         self.food_entry.delete(0, 'end')
         self.calories_entry.delete(0, 'end')
@@ -61,20 +65,46 @@ class Calculator:
 
     def calculate(self):
         results = tk.Tk()
-        results.geometry("350x350")
         results.title("results")
+
+        total_macros_header = ttk.Label(results, text="Total macros:")
+        total_macros_header.grid(row=0, column=0, padx=5, pady=5)
+
         calories_info = ttk.Label(results, text=f'Calories = {self.total_macros[0]}')
-        calories_info.grid(row=0, column=0, padx=10, pady=5)
+        calories_info.grid(row=1, column=0, padx=10, pady=5)
 
         protein_info = ttk.Label(results, text=f'protein = {self.total_macros[1]}')
-        protein_info.grid(row=1, column=0, padx=10, pady=5)
+        protein_info.grid(row=2, column=0, padx=10, pady=5)
 
         carbs_info = ttk.Label(results, text=f'carbs = {self.total_macros[2]}')
-        carbs_info.grid(row=2, column=0, padx=7, pady=5)
+        carbs_info.grid(row=3, column=0, padx=7, pady=5)
 
         fats_info = ttk.Label(results, text=f'fats = {self.total_macros[3]}')
-        fats_info.grid(row=3, column=0, padx=5, pady=5)
+        fats_info.grid(row=4, column=0, padx=5, pady=5)
 
+        header = ttk.Label(results, text="Food and Nutrients:")
+        header.grid(row=5, column=0, padx=5, pady=5)
+
+        count = 0
+
+        for food, macros in self.food_nutrients_dict.items():
+
+            food_header = ttk.Label(results, text=f'{food}')
+            food_header.grid(row=6, column=0 + count, padx=5, pady=5)
+
+            calorie_header = ttk.Label(results, text=f'calories = {macros[0]}')
+            calorie_header.grid(row=7, column=0 + count, padx=5, pady=5)
+
+            protein_header = ttk.Label(results, text=f'protein = {macros[1]}')
+            protein_header.grid(row=8, column=0 + count, padx=5, pady=5)
+
+            carb_header = ttk.Label(results, text=f'carbs = {macros[2]}')
+            carb_header.grid(row=9, column=0 + count, padx=5, pady=5)
+
+            fats_header = ttk.Label(results, text=f'fats = {macros[3]}')
+            fats_header.grid(row=10, column=0 + count, padx=5, pady=5)
+
+            count += 1
 
 
 Calculator()
